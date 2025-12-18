bdeb29c1b9a74eea8ca80300ef0562ddf10e6287
kyooni18 <kyoung01122189@gmail.com>
2025-12-18 16:51:42 +0900

    venv and embeds

---
+++ Embeddings.py
@@ -0,0 +1,22 @@
+class Embed:
+	def __init__(self):
+		self.model = None
+		self.dim = 1024
+	
+	def load(self):
+		from sentence_transformers import SentenceTransformer
+		import numpy as np
+		if self.model == None:
+			self.model = SentenceTransformer("BAAI/bge-m3")
+	
+	def embed(self, text):
+		if self.model == None:
+			self.load()
+		
+		vec = self.model.encode(
+			[text],
+			batch_size=32,
+			normalize_embeddings=True,
+			truncate_dim=self.dim)[0]
+		
+		return vec
